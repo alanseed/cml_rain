972b00b37859a564a14d511d4abfaf486d037eac
alanseed <alan.seed@gmail.com>
2024-11-09 09:58:01 +1100

    add reference power

--- scripts/db_utils.py
+++ scripts/db_utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from datetime import datetime, timedelta
+import pandas as pd
 import pymongo
 import pymongo.collection
 
@@ -42,3 +44,53 @@ def get_cmls(
     # Convert the list of records to a DataFrame
     cml_df = pd.DataFrame(records)
     return cml_df
+
+def is_valid_power(power: float) -> bool:
+    """
+    Check that the link power is within a valid range
+
+    Args:
+        power (float): Link power to be checked
+
+    Returns:
+        bool: True if within the range 
+    """
+    # Valid range for pmax or pmin based on the PDF of the Netherlands link data
+    max_valid_power = -20
+    min_valid_power = -70
+    if (power >= min_valid_power) & (power <= max_valid_power):
+        return True
+    else:
+        return False
+    
+def calc_max_pmin(
+    link_id: int, data_col: pymongo.collection.Collection, time: datetime
+) -> float:
+    """
+    Calculate the maximum valid Pmin in the last 24 hours
+    Args:
+        link_id (int): _description_
+        data_col (pymongo.collection.Collection): _description_
+        time (datetime): _description_
+    """
+    ref_power = float("NaN")
+    min_number_records = 25
+    start_time = time - timedelta(days=1)
+    query = {"link_id": link_id, "end_time": {"$gte":start_time, "$lte":time}}
+    projection = {"pmin.value":1, "_id":0}
+    number_records = data_col.count_documents(
+        filter=query
+    )
+    if number_records > min_number_records:
+        pmin = []
+        for doc in data_col.find(filter=query, projection = projection):
+            value = float(doc["pmin"]["value"])
+            if is_valid_power(value):
+                pmin.append(value)
+        
+        # calculate the max if we have enough valid values
+        if pmin:   
+            data = np.array(pmin, dtype=float)
+            if len(data) > min_number_records:
+                ref_power = data.max()
+    return ref_power
---
+++ scripts/reference_power.py
@@ -0,0 +1,134 @@
+"""
+Generate the reference power for each day 
+
+Reference power is defined as the maximum valid Pmin for the last 24 hours
+
+"""
+
+import sys
+
+sys.path.append("../scripts")
+import concurrent.futures
+
+from datetime import datetime, timedelta
+import argparse
+import os
+import numpy as np
+import pymongo
+import pymongo.collection
+import pandas as pd
+
+from db_utils import get_cmls, is_valid_power, calc_max_pmin
+
+
+def valid_date(s: str) -> np.datetime64:
+    """
+    Validate and parse a date string.
+
+    Args:
+        s (str): The date string to validate.
+
+    Returns:
+        np.datetime64: The parsed datetime object.
+
+    Raises:
+        argparse.ArgumentTypeError: If the date string is not valid.
+    """
+    try:
+        return np.datetime64(s)
+    except ValueError as e:
+        raise argparse.ArgumentTypeError(f"Not a valid date: {s!r}") from e
+
+
+def calculate_ref_power(
+    cml: dict,
+    data_col: pymongo.collection.Collection,
+    start_time: np.datetime64,
+    end_time: np.datetime64,
+):
+
+    link_id = cml["link_id"]
+    start_time_dt = pd.to_datetime(start_time).to_pydatetime()
+    end_time_dt = pd.to_datetime(end_time).to_pydatetime()
+    time_range = pd.date_range(start=start_time_dt, end=end_time_dt, freq="15min")
+
+    # loop over the time steps
+    for time in time_range:
+
+        # check that there is a record at this link for this time
+        has_record = data_col.count_documents(
+            filter={"link_id": link_id, "end_time": time}
+        )
+        if has_record == 1:
+            # check if there is rain along this link
+            max_pmin = calc_max_pmin(link_id, data_col, time)
+            max_pmin_doc = {"max_pmin": max_pmin}
+
+            # append rain_doc to the record
+            data_col.update_one(
+                {"link_id": link_id, "end_time": time},
+                {"$push": max_pmin_doc},
+                upsert=True,
+            )
+
+
+def main():
+    """Calculate the maximum valid Pmin over a 24 h period"""
+    parser = argparse.ArgumentParser(
+        description="Calculate the maximum valid Pmin over 24 h",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-s", "--start", type=valid_date, help="Start date yyyy-mm-dd")
+    parser.add_argument("-e", "--end", type=valid_date, help="End date yyyy-mm-dd")
+    args = parser.parse_args()
+
+    # print out some info
+    start_time = args.start
+    end_time = args.end
+    print(f"Start date = {start_time}\nEnd date = {end_time}")
+
+    # set up a local database
+    uri_str = "mongodb://localhost:27017"
+
+    # setup a MongoDB Atlas database
+    # usr = os.getenv("MONGO_USR")
+    # pwd = os.getenv("MONGO_PWD")
+    # if usr is None:
+    #     print("Valid MongoDB user not found", file=sys.stderr)
+    #     sys.exit(1)
+    # if pwd is None:
+    #     print("Valid MongoDB user password not found", file=sys.stderr)
+    #     sys.exit(1)
+    # uri_str = f"mongodb+srv://{usr}:{pwd}@wrnz.kej834t.mongodb.net/?retryWrites=true&w=majority"
+
+    myclient = pymongo.MongoClient(uri_str)
+    db = myclient["cml"]
+    cml_col = db["links"]
+    data_col = db["test_data"]
+
+    # get the list of cmls in the links dictionary in the area that we are working with
+    longitude = 4.0
+    latitude = 52.0
+    max_range = 250000
+    cmls = get_cmls(cml_col, longitude, latitude, max_range)
+    links = cmls.to_dict(orient="records")
+
+    # process the links in parallel
+    num_workers = 32  # Number of cores
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+        futures = [
+            executor.submit(calculate_ref_power, link, data_col, start_time, end_time)
+            for link in links
+        ]
+
+    # Ensure all threads complete by checking the results
+    for future in futures:
+        future.result()
+
+    # for link in links:
+    #     print(f"Processing link {link["link_id"]}")
+    #     calculate_ref_power(link, data_col, start_time, end_time)
+
+
+if __name__ == "__main__":
+    main()
