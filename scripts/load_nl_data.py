e474b230695262a58b97a5db708752d69e2dec64
alanseed <alan.seed@gmail.com>
2024-11-10 12:21:00 +1100

    improved design

--- scripts/load_nl_data.py
+++ scripts/load_nl_data.py
@@ -1,15 +1,66 @@
+import sys
+
+sys.path.append("../scripts")
+
 from pathlib import Path
 import pandas as pd
 from pymongo import MongoClient
 import numpy as np
 import pymongo
+import math
+from db_utils import is_valid_power 
+
+def write_data_records(data_df, data_col):
+    records = []
+    batch_size = 10000       
+    for index, row in data_df.iterrows():
+        p_min = row["Pmin"]
+        if not is_valid_power(p_min):
+            p_min = float("nan")
+
+        p_max = row["Pmax"]
+        if not is_valid_power(p_max):
+            p_max = float("nan")
+
+        # Only append records where both p_min and p_max are valid
+        if not (math.isnan(p_max) or math.isnan(p_min)):
+            record = {
+                "link_id": row["ID"],
+                "time":{ 
+                    "start_time": row["DateTime"] - time_step,
+                    "end_time": row["DateTime"]
+                },
+                "power":{
+                    "p_min": p_min,  # Use computed p_min
+                    "p_max": p_max,  # Use computed p_max
+                },
+                "atten":{
+                    "p_ref": float("NaN"),
+                    "has_rain": False,
+                    "atten": float("NaN"),
+                    "s_atten": float("NaN")
+                }
+            }
+            records.append(record) 
+
+            # Insert in batches of batch_size
+            if len(records) >= batch_size:
+                data_col.insert_many(records)
+                records = []  # Clear after batch insert
+
+    # Insert any remaining records
+    if records:
+        data_col.insert_many(records)
+
+    return
+
 
 # MongoDB connection (Local)
 client = MongoClient("mongodb://localhost:27017/")
 db = client["cml"]
 
 # Drop existing collections (if needed)
-link_col = db["links"]
+link_col = db["cml_metadata"]
 link_col.drop()
 
 # Assume 15 min time steps 
@@ -17,7 +68,7 @@ time_step = np.timedelta64(15,'m')
 
 # Read the files into dataframes
 file_names = ["CMLs_20110609_20110911_21days.dat", "CMLs_20120530_20120901.dat"]
-file_path = Path("/home/alanseed/cml_rain/data", file_names[0])
+file_path = Path("/home/alanseed/alan/cml_rain/data", file_names[0])
 data_df = pd.read_csv(file_path, sep=" ", header=0)
 data_df["DateTime"] = pd.to_datetime(data_df["DateTime"], format="%Y%m%d%H%M")
 
@@ -58,65 +109,26 @@ links = data_df_unique.apply(
 ).tolist()
 
 # Insert the links into MongoDB
-link_col.insert_many(links)
+link_col.insert_many(links) 
+print(f"Found {len(links)} stations")
 
 # Write out the time series data
-data_col = db["data"]
+data_col = db["cml_data"]
 data_col.drop()
-
-# Prepare to batch insert records
-records = []
-batch_size = 1000
-for index, row in data_df.iterrows():
-    record = {
-        "link_id": row["ID"],
-        "start_time":row["DateTime"] - time_step,
-        "end_time": row["DateTime"],
-        "pmin": {"value": row["Pmin"], "units": "dBm"},
-        "pmax": {"value": row["Pmax"], "units": "dBm"},  # Corrected typo
-    }
-    records.append(record)
-
-    # Insert in batches of 1000 records
-    if len(records) >= batch_size:
-        data_col.insert_many(records)
-        records = []
-
-# Insert any remaining records that didn’t fill the final batch
-if records:
-    data_col.insert_many(records)
+write_data_records(data_df, data_col)
 
 # now process the second file
-file_path = Path("/home/alanseed/cml_rain/data", file_names[1])
+file_path = Path("/home/alanseed/alan/cml_rain/data", file_names[1])
 print(f"Reading {file_path}")
 data_df = pd.read_csv(file_path, sep=" ", header=0)
 data_df["DateTime"] = pd.to_datetime(data_df["DateTime"], format="%Y%m%d%H%M")
-
-records = []
-for index, row in data_df.iterrows():
-    record = {
-        "link_id": row["ID"],
-        "start_time": row["DateTime"] - time_step,
-        "end_time": row["DateTime"],
-        "pmin": {"value": row["Pmin"], "units": "dBm"},
-        "pmax": {"value": row["Pmax"], "units": "dBm"}, 
-    }
-    records.append(record)
-
-    # Insert in batches of 1000 records
-    if len(records) >= batch_size:
-        data_col.insert_many(records)
-        records = []
-
-# Insert any remaining records that didn’t fill the final batch
-if records:
-    data_col.insert_many(records)
+write_data_records(data_df, data_col)
 
 # Set up the indexes
 data_index_id = pymongo.IndexModel(
     [("link_id", pymongo.ASCENDING)], name="link_id")
 data_index_id_time = pymongo.IndexModel(
-    [("link_id", pymongo.ASCENDING), ("end_time", pymongo.ASCENDING)], name="link_id_time"
+    [("link_id", pymongo.ASCENDING), ("time.end_time", pymongo.ASCENDING)], name="link_id_time"
 )
 data_col.create_indexes([data_index_id, data_index_id_time])
 
